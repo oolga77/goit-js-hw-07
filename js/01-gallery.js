b85316dfc2ea0fc99777bef088c35bd2dd25c218
oolga77 <oemi.ua@gmail.com>
2023-01-27 03:13:39 +0200

    required js, html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems)
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', onGalleryListClick)
+
+function createGalleryMarkup(galleryItems) {
+
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+}).join('');
+//console.log(markup);
+
+}
+
+
+function onGalleryListClick(evt) {
+  const isGalleryImg = evt.target.classList.contains('gallery__image')
+  if(!isGalleryImg){
+    return
+  }
+
+  evt.preventDefault();
+
+
+function onEscKeyPress(event){
+  
+if (event.code === "Escape"){
+  instance.close();
+}
+}
+
+const instance = basicLightbox.create(`
+<img src="${evt.target.dataset.source}">
+`, {
+  onShow: (instance) => {galleryList.addEventListener('keydown', onEscKeyPress)},
+  onClose: (instance) => {galleryList.removeEventListener('keydown', onEscKeyPress)}
+} )
+instance.show();
+
+}
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+
+return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+}).join('');
+//console.log(markup);
+
+}
+
+function imgCancelDownload(evt) {
+
+    evt.preventDefault();}
+
+    let lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: "alt",
+        captionDelay: 250,
+        
+      });
+
+  
